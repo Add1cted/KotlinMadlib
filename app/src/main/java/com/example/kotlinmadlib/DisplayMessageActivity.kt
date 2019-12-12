a833d3d14848fbfd93b629a661a1fc7ae540e04b
Mitchell Melrose <wmm18a@acu.edu>
2019-12-11 19:59:57 -0600

    finish

--- app/src/main/java/com/example/kotlinmadlib/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/kotlinmadlib/DisplayMessageActivity.kt
@@ -1,22 +1,34 @@
 package com.example.kotlinmadlib
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_display_message.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class DisplayMessageActivity : AppCompatActivity() {
-
     @SuppressLint("WrongConstant")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
-
-        val message = intent.getStringExtra(EXTRA_MESSAGE)
-
-        val textView = findViewById<TextView>(R.id.textView).apply {
-            text = message
-        }
+        val adjectiveOne = intent.getStringExtra("ADJECTIVE_1")
+        adjective1.text = "It was a " + adjectiveOne + ", cold November day."
+        val adjectiveTwo = intent.getStringExtra("ADJECTIVE_2")
+        adjective2.text = "I woke up to the " + adjectiveTwo + " smell of "
+        val nounOne = intent.getStringExtra("NOUN_1")
+        noun1.text = nounOne + " roasting in the "
+        val nounTwo = intent.getStringExtra("NOUN_2")
+        noun2.text = nounTwo + " downstairs."
+        val verbOne = intent.getStringExtra("VERB_1")
+        verb1.text = "I " + verbOne + " down the stairs to see "
+        val verbTwo = intent.getStringExtra("VERB_2")
+        verb2.text = " if I could " + verbTwo + " the dinner."
+        val nounThree = intent.getStringExtra("NOUN_3")
+        noun3.text = "My mom said, 'See if " +nounThree
+        val nounFour = intent.getStringExtra("NOUN_4")
+        noun4.text = "needs a fresh " +nounFour +"'."
     }
 }
--- app/src/main/java/com/example/kotlinmadlib/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmadlib/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.provider.AlarmClock
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,10 +17,36 @@ class MainActivity : AppCompatActivity() {
 
     fun sendMessage(view: View) {
         val editText = findViewById<EditText>(R.id.edit_message)
+        val editText2 = findViewById<EditText>(R.id.edit_message2)
+        val editText3 = findViewById<EditText>(R.id.edit_message3)
+        val editText4 = findViewById<EditText>(R.id.edit_message4)
+        val editText5 = findViewById<EditText>(R.id.edit_message5)
+        val editText6 = findViewById<EditText>(R.id.edit_message6)
+        val editText7 = findViewById<EditText>(R.id.edit_message7)
+        val editText8 = findViewById<EditText>(R.id.edit_message8)
         val message = editText.text.toString()
-        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
-            putExtra(AlarmClock.EXTRA_MESSAGE, message)
+        val message2 = editText2.text.toString()
+        val message3 = editText3.text.toString()
+        val message4 = editText4.text.toString()
+        val message5 = editText5.text.toString()
+        val message6 = editText6.text.toString()
+        val message7 = editText7.text.toString()
+        val message8 = editText8.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java)
+        intent.putExtra("ADJECTIVE_1", message)
+        intent.putExtra("ADJECTIVE_2", message2)
+        intent.putExtra("NOUN_1", message3)
+        intent.putExtra("NOUN_2", message4)
+        intent.putExtra("VERB_1", message5)
+        intent.putExtra("VERB_2", message6)
+        intent.putExtra("NOUN_3", message7)
+        intent.putExtra("NOUN_4", message8)
+        if(message.trim().isEmpty()||message2.trim().isEmpty()||message3.trim().isEmpty()||message4.trim().isEmpty()||message5.trim().isEmpty()||message6.trim().isEmpty()||message7.trim().isEmpty()||message8.trim().isEmpty()){
+            val toast = Toast.makeText(this, "Please fill out all fields!", Toast.LENGTH_SHORT)
+            toast.show()
         }
-        startActivity(intent)
+        else {
+            startActivity(intent)
+        }
     }
 }
